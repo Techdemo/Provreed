1d79fece2bf24551fee902393f4d265f2946c428
Marcel <fleuren.marcel@gmail.com>
2020-05-04 11:46:52 +0200

    added login form

--- client/src/pages/LoginPage/index.js
+++ client/src/pages/LoginPage/index.js
@@ -1,32 +1,39 @@
 import React from 'react';
-import { useForm } from "react-hook-form";
-
+import Title from '../../components/Title/index';
 import { makeStyles } from '@material-ui/core/styles';
 
-import Title from '../../components/Title/index'
+import { useForm } from "react-hook-form";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    '& > *': {
+    '& .MuiTextField-root': {
       margin: theme.spacing(1),
       width: '25ch',
-      display: 'flex',
-      flexDirection: 'column',
-    },
+    }
   },
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-evenly'
+  }
 }));
 
 const LoginPage = () => {
   const classes = useStyles();
   const { register, handleSubmit, watch, errors } = useForm();
-  const onSubmit = data => console.log(data);
+
+  const onSubmit = data => {
+    console.log('data submitted', data)
+  }
 
   return (
     <>
       <Title string="Login pagina" />
-      <form className={classes.root} autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
+      <form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
         <input name="username" ref={register({ required: true })} />
         <input name="password" ref={register({ required: true })} />
+        {errors.username && <span>username is required</span>}
+        {errors.password && <span>password is required</span>}
         <input type="submit" />
       </form>
     </>
